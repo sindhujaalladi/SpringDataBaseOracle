700db6b6b420f7bf42969cdfe3691a12fcd6f9cb
sindhujaalladi <sindhu.alladi1993@gmail.com>
2022-11-01 12:58:37 -0500

    iital commit

---
+++ src/main/java/com/springoracledb/SpringDataBaseOracleApplication.java
@@ -0,0 +1,13 @@
+package com.springoracledb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataBaseOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataBaseOracleApplication.class, args);
+	}
+
+}
